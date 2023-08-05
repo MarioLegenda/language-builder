288f065f535864f58aaae1be5cff62b34a573fa7
Mario <marioskrlec222@gmail.com>
2023-08-05 07:52:44 +0200

    Added quick add button for cards on deck

--- src/features/decks/list/Root.tsx
+++ src/features/decks/list/Root.tsx
@@ -1,4 +1,5 @@
-import { Button } from '@mantine/core';
+import {Button, Tooltip} from '@mantine/core';
+import {IconCirclePlus} from '@tabler/icons';
 import { useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import { Listing } from '@/features/shared/components/Listing';
@@ -7,6 +8,8 @@ import { FirestoreMetadata } from '@/lib/dataSource/firebase/firestoreMetadata';
 import { QueryKeys } from '@/lib/dataSource/queryKeys';
 import { useDeleteDocument } from '@/lib/dataSource/useDeleteDocument';
 import { useListDocuments } from '@/lib/dataSource/useListDocuments';
+
+import * as utilStyles from '@/styles/shared/Util.styles';
 export function Root() {
 	const { isFetching, isRefetching, data } = useListDocuments<DeckWithID>(QueryKeys.DECK_LISTING, 'decks');
 	const { mutateAsync, invalidateRelated } = useDeleteDocument();
@@ -15,7 +18,12 @@ export function Root() {
 		if (data) {
 			return data.map((item) => (
 				<tr key={item.name}>
-					<td>{item.name}</td>
+					<td css={utilStyles.flex('space-between', 'auto', 'center')}>{item.name}
+						<Tooltip color="gray" label="Create a card for this deck">
+							<Link to="/admin/cards/create">
+								<IconCirclePlus />
+							</Link>
+						</Tooltip></td>
 					<td>{item.language}</td>
 
 					<td>
